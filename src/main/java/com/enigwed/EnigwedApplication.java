cd5ce34ddeafb1160ae52ba9d60a64e7fd4c0171
AFRIZ <muhamadafrizalf@gmail.com>
2024-10-30 09:19:11 +0700

    [INITIAL COMMIT]

---
+++ src/main/java/com/enigwed/EnigwedApplication.java
@@ -0,0 +1,13 @@
+package com.enigwed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnigwedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigwedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enigwed/EnigwedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enigwed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnigwedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
