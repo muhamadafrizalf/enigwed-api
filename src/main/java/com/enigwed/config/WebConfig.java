23dec7173e52516d18b9000d51f6ed597f6640bb
AFRIZ <muhamadafrizalf@gmail.com>
2024-11-14 18:55:45 +0700

    [FIX] Image directory

--- src/main/java/com/enigwed/config/WebConfig.java
+++ src/main/java/com/enigwed/config/WebConfig.java
@@ -9,8 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Serve images from /app/images folder as static resources accessible via /images/
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:/home/enigma/asset/images/"); // Change path if using another directory
+                .addResourceLocations("file:/app/images/");
     }
 }
